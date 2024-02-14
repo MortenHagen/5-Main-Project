dd417526feba721f339e41618d203fd75fe1c5c7
MortenHagen <mortenhagen30@gmail.com>
2024-02-14 17:02:24 +0100

    firebase

---
+++ js/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2DsfGrZRY8DXuIYMOdunmopmEPQobBPE",
+  authDomain: "main-projects-ecba3.firebaseapp.com",
+  projectId: "main-projects-ecba3",
+  storageBucket: "main-projects-ecba3.appspot.com",
+  messagingSenderId: "701093816877",
+  appId: "1:701093816877:web:2fca07e8debf3578eaa6a4"
+};
+
+const app = initializeApp(firebaseConfig);
